75b8982a44ae262843be634103bb9f0e3c3426df
yangyue <daisysong19900215@gmail.com>
2022-09-09 22:27:56 -0700

    Feature 8 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 
 from django.urls import path, include
 from django.contrib.auth import authenticate, login
-from django.views.generic.base import LoginView
+from django.views.generic.base import LoginView, LogoutView
 
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- expenses/urls.py
+++ expenses/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("receipts/", include("receipts.urls")),
     path("", RedirectView.as_view(url=reverse_lazy("home"))),
-    path("accounts/login/", auth_views.LoginView.as_view(), name="login")
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout")
+
 ]
