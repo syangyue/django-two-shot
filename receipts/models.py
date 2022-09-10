01b6ae7d1744a4743d58c81b6d02a9497faf7e35
yangyue <daisysong19900215@gmail.com>
2022-09-09 22:21:08 -0700

    Feature 8 complete

--- receipts/models.py
+++ receipts/models.py
@@ -32,7 +32,7 @@ class Receipt(models.Model):
     vendor = models.CharField(max_length=200)
     total = models.DecimalField(decimal_places=3, max_digits=10)
     tax = models.DecimalField(decimal_places=3, max_digits=10)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(null=False)
     purchaser = models.ForeignKey(
         USER_MODEL,
         related_name="receipts",
--- receipts/views.py
+++ receipts/views.py
@@ -1,7 +1,11 @@
 from django.views.generic.list import ListView
 from .models import Receipt
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ReceiptListView(ListView):
+class ReceiptListView(LoginRequiredMixin, ListView):
     model = Receipt
     template_name = "receipt/list.html"
+
+    def get_queryset(self):
+        return Receipt.objects.filter(purchaser=self.request.user)
